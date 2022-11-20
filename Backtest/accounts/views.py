b0be0386b509969c438097f80ae025a45cbd8f3c
SJPark97 <tjwlsdud33@naver.com>
2022-11-20 14:55:08 +0900

    fix [B-006]: user 프로필

--- Backtest/accounts/views.py
+++ Backtest/accounts/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.contrib.auth import get_user_model
+
 # Authentication Decorators
 # from rest_framework.decorators import authentication_classes
 # permission Decorators
@@ -8,27 +9,20 @@ from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
 
 from rest_framework import status
-from django.shortcuts import get_object_or_404, get_list_or_404
+from django.shortcuts import get_object_or_404
 from .serializers import ProfileSerializer
 from .models import Profile
 
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'POST'])
 def my_profile(request):
-    profile = get_object_or_404(Profile, user=request.user.id)
-    serializer = ProfileSerializer(profile)
-    return Response(serializer.data)
-
-
-@api_view(['GET', 'POST', 'PUT'])
-def user_profile(request, user_id):
     if request.method == 'POST':
         serializer = ProfileSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
     elif request.method == 'GET':
-        profile = get_object_or_404(Profile, user=user_id)
+        profile = get_object_or_404(Profile, user=request.user.id)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
     elif request.method == 'PUT':
@@ -36,6 +30,13 @@ def user_profile(request, user_id):
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data)
+
+
+@api_view(['GET'])
+def user_profile(request, user_id):
+    profile = get_object_or_404(Profile, user=user_id)
+    serializer = ProfileSerializer(profile)
+    return Response(serializer.data)
 
 
 @api_view(['POST'])
